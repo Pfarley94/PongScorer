569b7d130a542b17a3a58c4d02da88b6bd87d769
Patrick Farley <patrickfarley1994@yahoo.com>
2019-07-16 22:31:26 -0400

    counts both scores till 20

---
+++ main.js
@@ -0,0 +1,90 @@
+console.log('helloworld!')
+
+var patsScore = 0;
+var nicksScore = 0;
+var gamePoint = 20;
+
+// code for the basic click function of the app
+$('#givePoint1').click(function () {
+    if (patsScore < gamePoint) {
+        document.getElementById('patsCurrentScore').textContent = patsScore += 1
+    } else {
+        (patsScore > gamePoint)
+        $('#patWins').prepend('');
+
+    }
+});
+
+$('#givePoint2').click(function () {
+    if (nicksScore < gamePoint) {
+        document.getElementById('nicksCurrentScore').textContent = nicksScore += 1;
+        
+    } else {
+        (nicksScore > gamePoint)
+        $('#nickWins').prepend('')
+    }
+});
+// // code for removing an accidental point
+// $('#givePoint1').dblclick(function () {
+//         document.getElementById('patsCurrentScore').textContent = patsScore -= 1
+
+// code for reloading the page before victory
+var pressHoldDuration = 100;
+// The item (or items) to press and hold on
+var item = document.querySelector("#");
+
+var timerID;
+var counter = 0;
+
+var pressHoldEvent = new CustomEvent("pressHold");
+
+// Increase or decreae value to adjust how long
+// one should keep pressing down before the pressHold
+// event fires
+// Listening for the mouse and touch events    
+item.addEventListener("mousedown", pressingDown, false);
+item.addEventListener("mouseup", notPressingDown, false);
+item.addEventListener("mouseleave", notPressingDown, false);
+
+item.addEventListener("touchstart", pressingDown, false);
+item.addEventListener("touchend", notPressingDown, false);
+
+// Listening for our custom pressHold event
+item.addEventListener("pressHold", doSomething, false);
+
+function pressingDown(e) {
+    // Start the timer
+    requestAnimationFrame(timer);
+
+    e.preventDefault();
+
+    console.log("Pressing!");
+}
+
+function notPressingDown(e) {
+    // Stop the timer
+    cancelAnimationFrame(timerID);
+    counter = 0;
+
+    console.log("Not pressing!");
+}
+
+//
+// Runs at 60fps when you are pressing down
+//
+function timer() {
+    console.log("Timer tick!");
+
+    if (counter < pressHoldDuration) {
+        timerID = requestAnimationFrame(timer);
+        counter++;
+    } else {
+        console.log("Press threshold reached!");
+        item.dispatchEvent(pressHoldEvent);
+    }
+}
+
+function doSomething(e) {
+    window.location.reload()
+    console.log("pressHold event fired!");
+}
